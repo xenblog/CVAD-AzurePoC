3ca89fc2efad329578ac62b2db807fdff174d020
Niels Holm <nch@edgemo.com>
2020-03-07 09:52:09 +0100

    master

--- CitricAzurePoc-script.ps1
+++ CitricAzurePoc-script.ps1
@@ -1,18 +1,21 @@
 $VmResourceGroupName = "RG_WEU_CloudConnector" # Read-Host -Prompt "Enter the Resource Group name" #RG_WEU_CloudConnector
 $location = "westeurope" # Read-Host -Prompt "Enter the location (i.e. centralus)"
+
+$vmName = "az-ccc01"
 $adminUsername = "azurelocaladmin" # Read-Host -Prompt "Enter the administrator username"
 $adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
 $dnsLabelPrefix = "az-ccc01" # Read-Host -Prompt "Enter an unique DNS name for the public IP"
 
 New-AzResourceGroup -Name $VmResourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
-    -ResourceGroupName $VmResourceGroupName `
-    -TemplateUri "https://raw.githubusercontent.com/xenblog/CVAD-AzurePoC/master/Templates/CitrixCloudConnector.json" `
-    -vmName "az-ccc01" `
+    -virtualMachineRG $VmResourceGroupName `
+    -TemplateUri "https://raw.githubusercontent.com/xenblog/CVAD-AzurePoC/master/Templates/template.json" `
+    -virtualMachineName $vmName `
     -adminUsername $adminUsername `
     -adminPassword $adminPassword `
+    -subnetName 'Infrastructure'
     -dnsLabelPrefix $dnsLabelPrefix `
     -windowsOSVersion '2019-Datacenter' `
-    -vmSize 'Standard_D2_v3'
+    -virtualMachineSize 'Standard_D2_v3'
 
  (Get-AzVm -ResourceGroupName $VmResourceGroupName).name
